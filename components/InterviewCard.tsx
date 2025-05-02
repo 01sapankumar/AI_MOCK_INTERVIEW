824b08a1458aefad132f8fdc027a83c279c39e99
01sapankumar <ksapan73@gmail.com>
2025-05-03 00:45:56 +0530

    Updated InterviewCard.tsx with latest improvements

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -8,7 +8,7 @@ import { getRandomInterviewCover } from '@/lib/utils';
 import DisplayTechIcons from '@/components/DisplayTechIcons';
 
 const InterviewCard = ({interviewId, userId, role, type, techstack, createdAt}: InterviewCardProps) => {
-  const feedback = null as Feedback null;
+  const feedback = null as Feedback | null;
   const normalizedtype = /mix/gi.test(type)? 'Mixed' : type ; 
   const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY');
   return (
@@ -19,8 +19,14 @@ const InterviewCard = ({interviewId, userId, role, type, techstack, createdAt}: 
           <div className='absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-600'>
             <p className='badge-text'>{normalizedtype}</p>
           </div>
-          <Image src = {getRandomInterviewCover()} alt= "cover-image" width={90} height = {90} className= "rounded-full 
-          object-fit size-[90px]" />
+          <Image 
+  src={getRandomInterviewCover()} 
+  alt="cover-image" 
+  width={90} 
+  height={90} 
+  className="rounded-full object-cover w-[90px] h-[90px]" 
+/>
+
           <h3 className=' mt-5 capitalized'> {role} Interview</h3>
           <div className='flex flex-row gap-5 mt-3'>
             <div className='flex flex-row gap-2'>
